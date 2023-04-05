7fcf2b09a7aa892cbfa66a9b72f20613a84010c0
Bessem Hamud <118807215+Bess-Hub@users.noreply.github.com>
2023-04-05 16:58:32 +0200

    Create Reto 3.py

---
+++ Reto 3.py
@@ -0,0 +1,17 @@
+# Will attempt to perform a homework from Ely that consists in creating a code capable of printing a sequence from 0 to 100 in reverse.
+
+# First create a list that consists of a range, since the last element is not inclusive of we add as it +1
+numbers = [*range (0, 100+1)]
+
+# Then print it to confirm all numbers are there
+print(numbers)
+print()
+
+# Now reverse the list.
+numbers.reverse()
+
+# Now print the reversed list, add a string to indicate it is a reversed list just out of personal preference
+print('Reversed List: ', numbers)
+print()
+
+# Exercise completed
