78911a7e6ae1518db42fa30a10ecc6f1228b3251
Bessem Hamud <118807215+Bess-Hub@users.noreply.github.com>
2023-04-06 09:12:14 +0200

    Update Reto 2.py

--- Reto 2.py
+++ Reto 2.py
@@ -1,14 +1,14 @@
-# Will attempt to perform a homework from Ely that consists in creating a code capable of printing all even numbers from a starting value to an end value
+# Will attempt to perform a homework from Ely that consists in creating a code capable of printing all odd numbers from a starting value to an end value
 
-## Reto 2
-### Escribe un programa capaz de mostrar todos los números impares desde un número de inicio y otro final.
-### Por ejemplo: teniendo numero_inicial = 2 y numero_final = 8, el programa debe imprimir por consola: [3, 5, 7]
+# Reto 2
+## Escribe un programa capaz de mostrar todos los números impares desde un número de inicio y otro final.
+## Por ejemplo: teniendo numero_inicial = 2 y numero_final = 8, el programa debe imprimir por consola: [3, 5, 7]
 
 # This is somehow similar to my Odd or Even exercise so I would start by requesting the user to present the range, the initial and the last number
 num_ini = int((input("Indicate a starting number:")))
 num_fin = int((input("Indicate an ending number:")))
 
-# The user inputs will then form a list
+# The user inputs will then form a list, since the num_fin is not inclusive I have added a +1, so it is included.
 numbers = [*range (num_ini, num_fin+1)]
 
 # We can test if it does by printing it
@@ -16,7 +16,7 @@ print("All numbers: ", numbers)
 print()
 
 # From all methods tried Comprehension was the simplest one
-even_list = [ i for i in numbers if i%2 == 0]
-print('Even numbers: ', even_list)
+odd_list = [ i for i in numbers if i%2 != 0]
+print('Even numbers: ', odd_list)
 
-#Success
+# Success
