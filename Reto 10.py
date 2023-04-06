6709562543001ad511836e570788d2c7b14c8b27
Bessem Hamud <118807215+Bess-Hub@users.noreply.github.com>
2023-04-06 10:32:17 +0200

    Create Reto 10.py

---
+++ Reto 10.py
@@ -0,0 +1,12 @@
+# Will attempt to perform a homework from Ely 
+
+# Reto 10
+## Escribe un programa que guarde en una variable el siguiente contenido:
+##{'titulo':'El Más Allá','aka':'E tu vivrai nel terrore - Laldilà','director':'Lucio Fulci', 'año':1981
+
+# To print as the exercise request it to be printed we should type the following:
+movie_detail = {'title':'El Más Allá', 'aka':'E tu vivrai nel terrore - Laldilà', 'director':'Lucio Fulci', 'year':'1981'}
+print(movie_detail)
+print()
+
+# Success
