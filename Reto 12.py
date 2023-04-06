21217912ded8263f8e10b13942de67fe9f2fbcd7
Bessem Hamud <118807215+Bess-Hub@users.noreply.github.com>
2023-04-06 10:58:13 +0200

    Update Reto 12.py

--- Reto 12.py
+++ Reto 12.py
@@ -1,6 +1,6 @@
 # Will attempt to perform a homework from Ely 
 
-# Reto 11
+# Reto 12
 ## Escribe un programa que almacene en una lista (Array) todos los nombres de los alumnos del curso Programación para No Programadores y los muestre en por pantalla.
 
 # First create a list of students
